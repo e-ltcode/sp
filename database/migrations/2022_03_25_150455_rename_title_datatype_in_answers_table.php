f78a53ed79ce94d54eee205577896c40d7614dbe
Ilyas <danish.ilyas786@gmail.com>
2022-03-25 20:11:41 +0500

    change datatype of title in answers table

--- database/migrations/2022_03_25_150455_rename_title_datatype_in_answers_table.php
+++ database/migrations/2022_03_25_150455_rename_title_datatype_in_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameQuizDescriptionInQuizesTable extends Migration
+class RenameTitleDatatypeInAnswersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class RenameQuizDescriptionInQuizesTable extends Migration
      */
     public function up()
     {
-        Schema::table('quizes', function (Blueprint $table) {
-            $table->text('quiz_description')->change();
+        Schema::table('answers', function (Blueprint $table) {
+            $table->text('title')->change();
         });
     }
 
